993e69052469a9c632a41170c8a5fa4b9cec0b56
CD4017BE <cd4017be@gmail.com>
2019-03-13 22:37:03 +0100

    log forced chunks on debug level to reduce log spam

--- src/java/cd4017be/dimstack/core/LoadingInfo.java
+++ src/java/cd4017be/dimstack/core/LoadingInfo.java
@@ -34,7 +34,7 @@ public class LoadingInfo {
 		if (pc.loadedChunks.size() >= n)
 			pc.unforceLeastRelevantChunks(n - 1);
 		ForgeChunkManager.forceChunk(ticket, chunk);
-		Main.LOG.info("forced chunk {} in {}", chunk, pc.dimId);
+		Main.LOG.debug("forced chunk {} in {}", chunk, pc.dimId);
 	}
 
 	public void onRequest(BlockPos from) {
@@ -46,14 +46,14 @@ public class LoadingInfo {
 	public LoadingInfo onUnload(boolean top) {
 		if (top) lastReqT = Long.MIN_VALUE;
 		else lastReqB = Long.MIN_VALUE;
-		Main.LOG.info("{} access of chunk {} in {} became unloaded", top ? "TOP" : "BOTTOM", chunk, pc.dimId);
+		Main.LOG.debug("{} access of chunk {} in {} became unloaded", top ? "TOP" : "BOTTOM", chunk, pc.dimId);
 		return this;
 	}
 
 	public boolean checkExpired(long t) {
 		if (lastReqB < t && lastReqT < t) {
 			ForgeChunkManager.unforceChunk(pc.loadingTicket, chunk);
-			Main.LOG.info("unforced chunk {} in {} after {}", chunk, pc.dimId, String.format("%.2fs", (float)(t - startTime + ChunkLoader.EXPIRE_TIME) / 1000F));
+			Main.LOG.debug("unforced chunk {} in {} after {}", chunk, pc.dimId, String.format("%.2fs", (float)(t - startTime + ChunkLoader.EXPIRE_TIME) / 1000F));
 			return true;
 		} else return false;
 	}
