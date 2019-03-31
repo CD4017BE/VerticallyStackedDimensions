86e57ca51bc5fb400240233745348634bda90a19
CD4017BE <cd4017be@gmail.com>
2019-03-31 18:34:06 +0200

    fix obf mappings

--- src/java/cd4017be/dimstack/asm/BlockPortalTransformer.java
+++ src/java/cd4017be/dimstack/asm/BlockPortalTransformer.java
@@ -35,10 +35,10 @@ public class BlockPortalTransformer implements IClassTransformer {
 	final String m_allowNetherPortal, md_allowNetherPortal;
 
 	public BlockPortalTransformer() {
-		c_World = type("net.minecraft.world.World", "ams");
+		c_World = type("net.minecraft.world.World", "amu");
 		c_BlockPos = type("net.minecraft.util.math.BlockPos", "et");
 		
-		c_BlockPortal = type("net.minecraft.block.BlockPortal", "asq");
+		c_BlockPortal = type("net.minecraft.block.BlockPortal", "ass");
 		n_BlockPortal = name(c_BlockPortal);
 		m_trySpawnPortal = method("trySpawnPortal", "b");
 		md_trySpawnPortal = m_desc(BOOL, c_World, c_BlockPos);
@@ -63,7 +63,7 @@ public class BlockPortalTransformer implements IClassTransformer {
 		LOG.debug("patching BlockPortal as {} ...", cn.name);
 		boolean found = false;
 		for (MethodNode mn : cn.methods) {
-			if (mn.desc.equals(md_trySpawnPortal) && m_trySpawnPortal.contains(mn.name)) {
+			if (mn.desc.equals(md_trySpawnPortal) && mn.name.equals(m_trySpawnPortal)) {
 				LOG.debug("patching method trySpawnPortal() as {}{}", mn.name, mn.desc);
 				InsnList inj = new InsnList();
 				
@@ -82,8 +82,10 @@ public class BlockPortalTransformer implements IClassTransformer {
 		}
 		if (!found) {
 			String[] names = new String[cn.methods.size()];
-			for (int i = 0; i < names.length; i++)
-				names[i] = cn.methods.get(i).name;
+			for (int i = 0; i < names.length; i++) {
+				MethodNode mn = cn.methods.get(i);
+				names[i] = mn.name + mn.desc;
+			}
 			LOG.error("can't find BlockPortal.trySpawnPortal() in {}", Arrays.toString(names));
 			LOG.info("method name = {}, descriptor = {}", m_trySpawnPortal, md_trySpawnPortal);
 		}
